ac14c2743b051948de619cc9553484a7547f593c
Alexander Sviridov <alexander_sviridov@icloud.com>
2015-11-27 11:28:00 +0300

    Initial Commit

---
+++ MySearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySearch
+//
+//  Created by Alexander Sviridov on 27/11/15.
+//  Copyright © 2015 Alexander Sviridov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySearch
+//
+//  Created by Alexander Sviridov on 27/11/15.
+//  Copyright © 2015 Alexander Sviridov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
